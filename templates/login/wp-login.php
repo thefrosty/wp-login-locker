c7df88b1137f255e50019a5bbc3a157af5a3c01e
Austin Passy <apassy@beachbody.com>
2018-06-14 14:39:34 -0700

    Add wp-login template.

---
+++ templates/login/wp-login.php
@@ -0,0 +1,7 @@
+<?php
+login_header(__('Log In'), '', ''); ?>
+    <p class="message"><?php esc_html_e('Login without a key has been disabled.', 'wp-login-locker'); ?></p>
+        </div>
+        <div class="clear"></div>
+    </body>
+</html>
