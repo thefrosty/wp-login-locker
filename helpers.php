b34e0545d13cd837e5cbbb9e3852a803be74d15b
Austin Passy <apassy@beachbody.com>
2018-06-15 11:43:33 -0700

    Remove unused functions.

--- helpers.php
+++ helpers.php
@@ -3,33 +3,6 @@
 namespace Dwnload\WpLoginLocker\Helpers;
 
 /**
- * Filters a variable with a boolean filter.
- * @param mixed $variable Incoming data.
- * @return bool
- */
-function filter_var_bool( $variable ) : bool {
-    return \filter_var( $variable, FILTER_VALIDATE_BOOLEAN );
-}
-
-/**
- * Filters a variable with an int filter.
- * @param mixed $variable Incoming data.
- * @return int
- */
-function filter_var_int( $variable ) : int {
-    return \filter_var( $variable, FILTER_VALIDATE_INT );
-}
-
-/**
- * Filters a variable with a string filter.
- * @param mixed $variable Incoming data.
- * @return string
- */
-function filter_var_string( $variable ) : string {
-    return \filter_var( $variable, FILTER_SANITIZE_STRING );
-}
-
-/**
  * Close the current session and terminate all scripts.
  */
 function terminate() {
