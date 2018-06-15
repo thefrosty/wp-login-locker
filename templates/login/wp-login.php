61dbf6c0f6696ccca1f7e06ba953496f6e949b85
Austin Passy <apassy@beachbody.com>
2018-06-15 11:46:24 -0700

    Update disabled login message to be filterable.

--- templates/login/wp-login.php
+++ templates/login/wp-login.php
@@ -1,7 +1,13 @@
 <?php
-login_header(__('Log In'), '', ''); ?>
-    <p class="message"><?php esc_html_e('Login without a key has been disabled.', 'wp-login-locker'); ?></p>
-        </div>
-        <div class="clear"></div>
-    </body>
+function_exists('login_header') || exit;
+login_header(__('Log In'), '', '');
+?>
+<p class="message"><?php
+    echo apply_filters(\Dwnload\WpLoginLocker\LoginLocker::HOOK_PREFIX . 'wp-login/message',
+        esc_html__('Login without a key has been disabled.', 'wp-login-locker'));
+    ?>
+</p>
+</div>
+<div class="clear"></div>
+</body>
 </html>
