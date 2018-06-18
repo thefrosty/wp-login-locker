982e045634a504bf7d060c87c9f2b3da8458e0de
Austin Passy <apassy@beachbody.com>
2018-06-18 16:00:22 -0700

    Change the class to a _setting_.

--- src/UserProfile/EmailNotificationSetting.php
+++ src/UserProfile/EmailNotificationSetting.php
@@ -2,13 +2,34 @@
 
 namespace Dwnload\WpLoginLocker\UserProfile;
 
+use Dwnload\WpLoginLocker\LoginLocker;
+
 /**
  * Class EmailNotification
  *
  * @package Dwnload\WpLoginLocker\UserProfile
  */
-class EmailNotification extends UserProfile
+class EmailNotificationSetting extends UserProfile
 {
+    /**
+     * EmailNotification constructor.
+     */
+    public function __construct()
+    {
+        $this->fields = [
+            LoginLocker::USER_EMAIL_META_KEY,
+        ];
+    }
+
+    /**
+     * Add class hooks.
+     */
+    public function addHooks()
+    {
+        $this->addAction(parent::USER_PROFILE_HOOK, [$this, 'showExtraUserFields']);
+        parent::addHooks();
+    }
+
     /**
      * Show extra user fields for last login IP and time.
      *
