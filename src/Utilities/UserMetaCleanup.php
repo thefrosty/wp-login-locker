6b62bbf4f53cb204c7c167e665730235c60ca07e
Austin Passy <367897+thefrosty@users.noreply.github.com>
2019-07-19 10:23:55 -0700

    New class to clean (delete) user meta on unique fields to only allow 10 entries.

---
+++ src/Utilities/UserMetaCleanup.php
@@ -0,0 +1,116 @@
+<?php declare(strict_types=1);
+
+namespace Dwnload\WpLoginLocker\Utilities;
+
+use Dwnload\WpLoginLocker\LoginLocker;
+
+/**
+ * Class UserMetaCleanup
+ *
+ * @package Dwnload\WpLoginLocker\Utilities
+ */
+class UserMetaCleanup
+{
+
+    /**
+     * User ID.
+     *
+     * @var int $user_id
+     */
+    private $user_id;
+
+    private const MAX_POST_META_COUNT = 10;
+
+    /**
+     * UserMetaCleanup constructor.
+     *
+     * @param int $user_id
+     */
+    public function __construct(int $user_id)
+    {
+        $this->user_id = $user_id;
+    }
+
+    /**
+     * Initiated the user meta cleaning.
+     */
+    public function cleanup()
+    {
+        $this->cleanupIp();
+        $this->cleanupTime();
+    }
+
+    /**
+     * Clean old IP meta.
+     */
+    private function cleanupIp()
+    {
+        $meta_ids = $this->query(LoginLocker::LAST_LOGIN_IP_META_KEY);
+
+        if (!\count($meta_ids) || \count($meta_ids) > self::MAX_POST_META_COUNT + 5) {
+            return;
+        }
+
+        $this->delete(\array_diff($meta_ids, $this->getSlice($meta_ids)));
+    }
+
+    /**
+     * Clean old time meta.
+     */
+    private function cleanupTime()
+    {
+        $meta_ids = $this->query(LoginLocker::LAST_LOGIN_TIME_META_KEY);
+
+        if (!\count($meta_ids) || \count($meta_ids) > self::MAX_POST_META_COUNT + 5) {
+            return;
+        }
+
+        $this->delete(\array_diff($meta_ids, $this->getSlice($meta_ids)));
+    }
+
+    /**
+     * Get the last X of the meta_id array.
+     *
+     * @param array $meta_ids
+     *
+     * @return array
+     */
+    private function getSlice(array $meta_ids): array
+    {
+        return \array_slice($meta_ids, -self::MAX_POST_META_COUNT, self::MAX_POST_META_COUNT, true);
+    }
+
+    /**
+     * Get the meta array from users.
+     *
+     * @param string $meta_key
+     *
+     * @return array
+     */
+    private function query(string $meta_key): array
+    {
+        global $wpdb;
+        $table = \_get_meta_table('user');
+        $query = $wpdb->prepare(
+            "SELECT umeta_id FROM $table WHERE user_id = %d AND meta_key = %s",
+            $this->user_id,
+            $meta_key
+        );
+
+        return $wpdb->get_col($query);
+    }
+
+    /**
+     * Delete the arrays past down from the diff.
+     *
+     * @param array $meta_ids
+     */
+    private function delete(array $meta_ids): void
+    {
+        global $wpdb;
+        $table = \_get_meta_table('user');
+        $query = "DELETE FROM $table WHERE umeta_id IN( " . \implode(',', $meta_ids) . ' )';
+
+        $wpdb->query($query);
+    }
+}
