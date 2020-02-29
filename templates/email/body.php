461191570a0a8e7c8f47d580c57d5c784a149941
Austin Passy <367897+thefrosty@users.noreply.github.com>
2020-02-29 09:14:22 -0800

    Update body of email template to no longer use ipsum and process text/template tags.

--- templates/email/body.php
+++ templates/email/body.php
@@ -7,7 +7,7 @@
 
     <!-- Visually Hidden Preheader Text : BEGIN -->
     <div style="display: none; font-size: 1px; line-height: 1px; max-height: 0px; max-width: 0px; opacity: 0; overflow: hidden; mso-hide: all; font-family: sans-serif;">
-        (Optional) This text will appear in the inbox preview, but not the email body. It can be used to supplement the email subject line or even summarize the email's contents. Extended text preheaders (~490 characters) seems like a better UX for anyone using a screenreader or voice-command apps like Siri to dictate the contents of an email. If this text is not included, email clients will automatically populate it using the text (including image alt text) at the start of the email's body.
+        {pretext}
     </div>
     <!-- Visually Hidden Preheader Text : END -->
 
@@ -33,98 +33,34 @@
         <!-- Email Body : BEGIN -->
         <table align="center" role="presentation" cellspacing="0" cellpadding="0" border="0" width="100%" style="margin: 0 auto;">
             <!-- Email Header : BEGIN -->
-            <tr>
-                <td style="padding: 20px 0; text-align: center">
-                    <img src="http://placehold.it/200x50" width="200" height="50" alt="alt_text" border="0" style="height: auto; background: #dddddd; font-family: sans-serif; font-size: 15px; line-height: 15px; color: #555555;">
-                </td>
-            </tr>
+<!--            <tr>-->
+<!--                <td style="padding: 20px 0; text-align: center">-->
+<!--                    <img src="http://placehold.it/200x50" width="200" height="50" alt="alt_text" border="0" style="height: auto; background: #dddddd; font-family: sans-serif; font-size: 15px; line-height: 15px; color: #555555;">-->
+<!--                </td>-->
+<!--            </tr>-->
             <!-- Email Header : END -->
 
             <!-- Hero Image, Flush : BEGIN -->
-            <tr>
-                <td style="background-color: #ffffff;">
-                    <img src="http://placehold.it/1200x600" width="600" height="" alt="alt_text" border="0" style="width: 100%; max-width: 600px; height: auto; background: #dddddd; font-family: sans-serif; font-size: 15px; line-height: 15px; color: #555555; margin: auto;" class="g-img">
-                </td>
-            </tr>
+<!--            <tr>-->
+<!--                <td style="background-color: #ffffff;">-->
+<!--                    <img src="http://placehold.it/1200x600" width="600" height="" alt="alt_text" border="0" style="width: 100%; max-width: 600px; height: auto; background: #dddddd; font-family: sans-serif; font-size: 15px; line-height: 15px; color: #555555; margin: auto;" class="g-img">-->
+<!--                </td>-->
+<!--            </tr>-->
             <!-- Hero Image, Flush : END -->
 
-            <!-- 1 Column Text + Button : BEGIN -->
+            <!-- 1 Column Text : BEGIN -->
             <tr>
                 <td style="background-color: #ffffff;">
                     <table role="presentation" cellspacing="0" cellpadding="0" border="0" width="100%">
                         <tr>
                             <td style="padding: 20px; font-family: sans-serif; font-size: 15px; line-height: 20px; color: #555555;">
-                                <h1 style="margin: 0 0 10px 0; font-family: sans-serif; font-size: 25px; line-height: 30px; color: #333333; font-weight: normal;">Praesent laoreet malesuada&nbsp;cursus.</h1>
-                                <p style="margin: 0;">Maecenas sed ante pellentesque, posuere leo id, eleifend dolor. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Praesent laoreet malesuada cursus. Maecenas scelerisque congue eros eu posuere. Praesent in felis ut velit pretium lobortis rhoncus ut&nbsp;erat.</p>
-                            </td>
-                        </tr>
-                        <tr>
-                            <td style="padding: 0 20px;">
-                                <!-- Button : BEGIN -->
-                                <table align="center" role="presentation" cellspacing="0" cellpadding="0" border="0" style="margin: auto;">
-                                    <tr>
-                                        <td class="button-td button-td-primary" style="border-radius: 4px; background: #222222;">
-                                            <a class="button-a button-a-primary" href="https://google.com/" style="background: #222222; border: 1px solid #000000; font-family: sans-serif; font-size: 15px; line-height: 15px; text-decoration: none; padding: 13px 17px; color: #ffffff; display: block; border-radius: 4px;">Primary Button</a>
-                                        </td>
-                                    </tr>
-                                </table>
-                                <!-- Button : END -->
-                            </td>
-                        </tr>
-                        <tr>
-                            <td style="padding: 20px; font-family: sans-serif; font-size: 15px; line-height: 20px; color: #555555;">
-                                <h2 style="margin: 0 0 10px 0; font-family: sans-serif; font-size: 18px; line-height: 22px; color: #333333; font-weight: bold;">Praesent in felis ut velit pretium lobortis rhoncus ut&nbsp;erat.</h2>
-                                <ul style="padding: 0; margin: 0 0 10px 0; list-style-type: disc;">
-                                    <li style="margin:0 0 10px 30px;" class="list-item-first">A list item.</li>
-                                    <li style="margin:0 0 10px 30px;">Another list item here.</li>
-                                    <li style="margin: 0 0 0 30px;" class="list-item-last">Everyone gets a list item, list items for everyone!</li>
-                                </ul>
-                                <p style="margin: 0;">Maecenas sed ante pellentesque, posuere leo id, eleifend dolor. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Praesent laoreet malesuada cursus. Maecenas scelerisque congue eros eu posuere. Praesent in felis ut velit pretium lobortis rhoncus ut&nbsp;erat.</p>
+                                <p style="margin: 0;">{message}</p>
                             </td>
                         </tr>
                     </table>
                 </td>
             </tr>
-            <!-- 1 Column Text + Button : END -->
-
-            <!-- 2 Even Columns : BEGIN -->
-            <tr>
-                <td height="100%" valign="top" width="100%" style="padding: 0 10px 40px 10px; background-color: #ffffff;">
-                    <table role="presentation" border="0" cellpadding="0" cellspacing="0" width="100%" style="max-width:560px;">
-                        <tr>
-                            <td valign="top" width="50%">
-                                <table role="presentation" cellspacing="0" cellpadding="0" border="0" width="100%" style="font-size: 14px; text-align: left;">
-                                    <tr>
-                                        <td style="text-align: center; padding: 0 10px;">
-                                            <img src="http://placehold.it/200" width="200" height="" alt="alt_text" border="0" style="width: 100%; max-width: 200px; background: #dddddd; font-family: sans-serif; font-size: 15px; line-height: 15px; color: #555555;">
-                                        </td>
-                                    </tr>
-                                    <tr>
-                                        <td style="text-align: left; font-family: sans-serif; font-size: 15px; line-height: 20px; color: #555555; padding: 10px 10px 0;">
-                                            <p style="margin: 0;">Maecenas sed ante pellentesque, posuere leo id, eleifend dolor.</p>
-                                        </td>
-                                    </tr>
-                                </table>
-                            </td>
-                            <td valign="top" width="50%">
-                                <table role="presentation" cellspacing="0" cellpadding="0" border="0" width="100%" style="font-size: 14px; text-align: left;">
-                                    <tr>
-                                        <td style="text-align: center; padding: 0 10px;">
-                                            <img src="http://placehold.it/200" width="200" height="" alt="alt_text" border="0" style="width: 100%; max-width: 200px; background: #dddddd; font-family: sans-serif; font-size: 15px; line-height: 15px; color: #555555;">
-                                        </td>
-                                    </tr>
-                                    <tr>
-                                        <td style="text-align: left; font-family: sans-serif; font-size: 15px; line-height: 20px; color: #555555; padding: 10px 10px 0;">
-                                            <p style="margin: 0;">Maecenas sed ante pellentesque, posuere leo id, eleifend dolor.</p>
-                                        </td>
-                                    </tr>
-                                </table>
-                            </td>
-                        </tr>
-                    </table>
-                </td>
-            </tr>
-            <!-- Two Even Columns : END -->
+            <!-- 1 Column Text : END -->
 
             <!-- Clear Spacer : BEGIN -->
             <tr>
@@ -134,35 +70,21 @@
             </tr>
             <!-- Clear Spacer : END -->
 
-            <!-- 1 Column Text : BEGIN -->
-            <tr>
-                <td style="background-color: #ffffff;">
-                    <table role="presentation" cellspacing="0" cellpadding="0" border="0" width="100%">
-                        <tr>
-                            <td style="padding: 20px; font-family: sans-serif; font-size: 15px; line-height: 20px; color: #555555;">
-                                <p style="margin: 0;">Maecenas sed ante pellentesque, posuere leo id, eleifend dolor. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Praesent laoreet malesuada cursus. Maecenas scelerisque congue eros eu posuere. Praesent in felis ut velit pretium lobortis rhoncus ut&nbsp;erat.</p>
-                            </td>
-                        </tr>
-                    </table>
-                </td>
-            </tr>
-            <!-- 1 Column Text : END -->
-
         </table>
         <!-- Email Body : END -->
 
         <!-- Email Footer : BEGIN -->
-        <table role="presentation" cellspacing="0" cellpadding="0" border="0" width="100%" style="max-width: 600px;">
-            <tr>
-                <td style="padding: 20px; font-family: sans-serif; font-size: 12px; line-height: 15px; text-align: center; color: #888888;">
-                    <webversion style="color: #cccccc; text-decoration: underline; font-weight: bold;">View as a Web Page</webversion>
-                    <br><br>
-                    Company Name<br><span class="unstyle-auto-detected-links">123 Fake Street, SpringField, OR, 97477 US<br>(123) 456-7890</span>
-                    <br><br>
-                    <unsubscribe style="color: #888888; text-decoration: underline;">unsubscribe</unsubscribe>
-                </td>
-            </tr>
-        </table>
+<!--        <table role="presentation" cellspacing="0" cellpadding="0" border="0" width="100%" style="max-width: 600px;">-->
+<!--            <tr>-->
+<!--                <td style="padding: 20px; font-family: sans-serif; font-size: 12px; line-height: 15px; text-align: center; color: #888888;">-->
+<!--                    <webversion style="color: #cccccc; text-decoration: underline; font-weight: bold;">View as a Web Page</webversion>-->
+<!--                    <br><br>-->
+<!--                    Company Name<br><span class="unstyle-auto-detected-links">123 Fake Street, SpringField, OR, 97477 US<br>(123) 456-7890</span>-->
+<!--                    <br><br>-->
+<!--                    <unsubscribe style="color: #888888; text-decoration: underline;">unsubscribe</unsubscribe>-->
+<!--                </td>-->
+<!--            </tr>-->
+<!--        </table>-->
         <!-- Email Footer : END -->
 
         <!--[if mso]>
@@ -185,7 +107,7 @@
                     <table role="presentation" cellspacing="0" cellpadding="0" border="0" width="100%">
                         <tr>
                             <td style="padding: 20px; text-align: left; font-family: sans-serif; font-size: 15px; line-height: 20px; color: #ffffff;">
-                                <p style="margin: 0;">Maecenas sed ante pellentesque, posuere leo id, eleifend dolor. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Praesent laoreet malesuada cursus. Maecenas scelerisque congue eros eu posuere. Praesent in felis ut velit pretium lobortis rhoncus ut&nbsp;erat.</p>
+                                <p style="margin: 0;">{pretext}</p>
                             </td>
                         </tr>
                     </table>
