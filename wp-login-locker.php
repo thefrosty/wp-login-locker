a62864f27089ea4aa206883d094e72fad0300d8a
Austin Passy <367897+thefrosty@users.noreply.github.com>
2020-04-21 10:05:38 -0700

    Update Symfony HTTP Foundation.

--- wp-login-locker.php
+++ wp-login-locker.php
@@ -4,7 +4,7 @@
  * Description: Disable direct access to your sites /wp-login.php script, plus user notifications based on actions.
  * Author: Austin Passy
  * Author URI: https://github.com/thefrosty
- * Version: 1.3.0
+ * Version: 1.3.1
  * Requires at least: 5.0
  * Tested up to: 5.3
  * Requires PHP: 7.2
