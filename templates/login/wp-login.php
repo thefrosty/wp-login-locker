b7141d3efead800e9f817f103399b136b6b00ed3
Austin Passy <367897+thefrosty@users.noreply.github.com>
2020-09-14 12:25:52 -0700

    phpcs cleanup.

--- templates/login/wp-login.php
+++ templates/login/wp-login.php
@@ -3,8 +3,10 @@ function_exists('login_header') || wp_die();
 login_header(__('Log In'), '', '');
 ?>
 <p class="message"><?php
-    echo apply_filters(TheFrosty\WpLoginLocker\LoginLocker::HOOK_PREFIX . 'wp-login/message',
-        esc_html__('Login without a key has been disabled.', 'wp-login-locker'));
+    echo apply_filters(
+        TheFrosty\WpLoginLocker\LoginLocker::HOOK_PREFIX . 'wp-login/message',
+        esc_html__('Login without a key has been disabled.', 'wp-login-locker')
+    );
     ?></p>
 </div>
 <div class="clear"></div>
