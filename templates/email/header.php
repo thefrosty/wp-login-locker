10587c81d8df31a8d268ceab1c7145f3592c2716
Austin Passy <367897+thefrosty@users.noreply.github.com>
2020-09-15 19:05:35 -0700

    Update email template with settings variables.

--- templates/email/header.php
+++ templates/email/header.php
@@ -1,4 +1,5 @@
-<?php // https://raw.githubusercontent.com/TedGoas/Cerberus/master/cerberus-fluid.html
+<?php // @phpcs:disable
+// @link https://raw.githubusercontent.com/TedGoas/Cerberus/master/cerberus-fluid.html
 ?><!DOCTYPE html>
 <html lang="en" xmlns="http://www.w3.org/1999/xhtml" xmlns:v="urn:schemas-microsoft-com:vml" xmlns:o="urn:schemas-microsoft-com:office:office">
 <head>
