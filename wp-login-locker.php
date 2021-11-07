7fc2a08eb01412171d767b52a08e591267fe717f
Austin Passy <367897+thefrosty@users.noreply.github.com>
2021-11-07 13:36:47 -0800

    Dependency updates (#38)
    * Remove asset source hack for WP Settings API, since asset are now loaded from CDN: https://github.com/dwnload/WpSettingsApi/pull/33
    
    * Remove bin shell files, which are now part of the wp-utilites package.
    
    * Bump PHP to 7.4.
    
    * Update build WordPress version, and development bin path.
    
    * Version 2.1.6.
    
    * Update dependencies.
    
    * Resolve:
    CVE-2021-3807
    moderate severity
    Vulnerable versions: > 2.1.1, < 5.0.1
    Patched version: 5.0.1
    
    ansi-regex is vulnerable to Inefficient Regular Expression Complexity
    
    * Update Travis to build PHP 7.4, and allow PHP 8.0 to fail.
    
    * Change composer bin path.
    
    * Try again
    
    * Update order and ls directory.
    
    * Try without bin path.
    
    * Use correct source for bin path.
    
    * Update scripts lib paths.
    
    * Missing trailing slash.
    
    * Update tests for remove action hooks.
    
    * Update package source bin path.

--- wp-login-locker.php
+++ wp-login-locker.php
@@ -4,10 +4,10 @@
  * Description: Disable direct access to your sites /wp-login.php script, plus user notifications based on actions.
  * Author: Austin Passy
  * Author URI: https://github.com/thefrosty
- * Version: 2.1.5
+ * Version: 2.1.6
  * Requires at least: 5.4
- * Tested up to: 5.5.1
- * Requires PHP: 7.3
+ * Tested up to: 5.8.1
+ * Requires PHP: 7.4
  * Plugin URI: https://github.com/thefrosty/wp-login-locker
  * GitHub Plugin URI: https://github.com/thefrosty/wp-login-locker
  * Primary Branch: develop
@@ -40,7 +40,7 @@ $plugin
     ->add(new Login\WpLogin())
     ->add(new Settings())
     ->add(new WpCore\WpSignup())
-    ->add(new WpSettingsApi(Settings::factory('2.1.2')))
+    ->add(new WpSettingsApi(Settings::factory('2.1.6')))
     ->addOnHook(Login\Login::class, 'login_init', 5)
     ->addOnHook(Login\LastLoginColumns::class, 'admin_init', 10, true)
     ->addOnHook(UserProfile\LastLogin::class, 'admin_init', 10, true)
