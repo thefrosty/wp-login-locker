ef093d99b413fb3189d3cd857cb71b117848185f
Austin Passy <367897+thefrosty@users.noreply.github.com>
2022-07-02 12:36:16 -0700

    Cleanup GEO code.

--- src/Utilities/GeoUtilTrait.php
+++ src/Utilities/GeoUtilTrait.php
@@ -5,7 +5,7 @@ namespace TheFrosty\WpLoginLocker\Utilities;
 use TheFrosty\WpUtilities\Plugin\HttpFoundationRequestTrait;
 
 /**
- * Class GeoUtil
+ * Trait GeoUtilTrait
  * @package TheFrosty\WpLoginLocker\Utilities
  */
 trait GeoUtilTrait
@@ -14,24 +14,24 @@ trait GeoUtilTrait
     use HttpFoundationRequestTrait;
 
     /**
-     * Get IP Address of user.w
+     * Get IP Address of user.
      * @return string
      */
     public function getIP(): string
     {
-        if ($this->getRequest()->server->has('HTTP_CLIENT_IP') &&
+        if (
+            $this->getRequest()->server->has('HTTP_CLIENT_IP') &&
             !empty($this->getRequest()->server->get('HTTP_CLIENT_IP'))
         ) {
-            $ip_address = $this->getRequest()->server->get('HTTP_CLIENT_IP');
-        } elseif ($this->getRequest()->server->has('HTTP_X_FORWARDED_FOR') &&
+            return \strval($this->getRequest()->server->get('HTTP_CLIENT_IP'));
+        } elseif (
+            $this->getRequest()->server->has('HTTP_X_FORWARDED_FOR') &&
             !empty($this->getRequest()->server->get('HTTP_X_FORWARDED_FOR'))
         ) {
-            $ip_address = $this->getRequest()->server->get('HTTP_X_FORWARDED_FOR');
-        } else {
-            $ip_address = $this->getRequest()->server->get('REMOTE_ADDR');
+            return \strval($this->getRequest()->server->get('HTTP_X_FORWARDED_FOR'));
         }
 
-        return \strval($ip_address);
+        return \strval($this->getRequest()->server->get('REMOTE_ADDR'));
     }
 
     /**
