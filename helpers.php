e1f401cace2ecf7b115c5d917e5f063b7d208278
Austin Passy <367897+thefrosty@users.noreply.github.com>
2023-11-01 14:57:52 -0700

    Add PHP 8 attribute.

--- helpers.php
+++ helpers.php
@@ -1,10 +1,15 @@
-<?php declare(strict_types=1);
+<?php
+
+declare(strict_types=1);
 
 namespace TheFrosty\WpLoginLocker\Helpers;
+
+use JetBrains\PhpStorm\NoReturn;
 
 /**
  * Close the current session and terminate all scripts.
  */
+#[NoReturn]
 function terminate(): void
 {
     \session_write_close();
