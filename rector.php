0a41530c2cda9561400935685a521516a1b156ad
Austin Passy <367897+thefrosty@users.noreply.github.com>
2025-04-14 16:16:48 -0700

    CLeanup

--- rector.php
+++ rector.php
@@ -4,16 +4,13 @@ declare(strict_types=1);
 
 use Rector\Config\RectorConfig;
 use Rector\PHPUnit\Set\PHPUnitSetList;
-use Rector\Set\ValueObject\SetList;
 
 return RectorConfig::configure()
     ->withPaths([
-//        __DIR__ . '/src',
         __DIR__ . '/tests',
     ])
     ->withSets([
-        PHPUnitSetList::PHPUNIT_110,
-        SetList::PHP_83,
+        PHPUnitSetList::PHPUNIT_110
     ])
     ->withTypeCoverageLevel(0)
     ->withDeadCodeLevel(0)
