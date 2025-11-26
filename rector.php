b5722e67dc7b2d56522db937697d01a6060d1dde
Austin Passy <367897+thefrosty@users.noreply.github.com>
2025-11-25 16:37:29 -0800

    Rector rules for PHPunit

--- rector.php
+++ rector.php
@@ -3,6 +3,13 @@
 declare(strict_types=1);
 
 use Rector\Config\RectorConfig;
+use Rector\PHPUnit\AnnotationsToAttributes\Rector\Class_\AnnotationWithValueToAttributeRector;
+use Rector\PHPUnit\AnnotationsToAttributes\Rector\Class_\CoversAnnotationWithValueToAttributeRector;
+use Rector\PHPUnit\AnnotationsToAttributes\Rector\Class_\RequiresAnnotationWithValueToAttributeRector;
+use Rector\PHPUnit\CodeQuality\Rector\StmtsAwareInterface\DeclareStrictTypesTestsRector;
+use Rector\PHPUnit\PHPUnit50\Rector\StaticCall\GetMockRector;
+use Rector\PHPUnit\PHPUnit60\Rector\ClassMethod\ExceptionAnnotationRector;
+use Rector\PHPUnit\PHPUnit90\Rector\MethodCall\ExplicitPhpErrorApiRector;
 use Rector\PHPUnit\Set\PHPUnitSetList;
 
 return RectorConfig::configure()
@@ -11,6 +18,15 @@ return RectorConfig::configure()
     ])
     ->withSets([
         PHPUnitSetList::PHPUNIT_110
+    ])
+    ->withRules([
+        AnnotationWithValueToAttributeRector::class,
+        CoversAnnotationWithValueToAttributeRector::class,
+        DeclareStrictTypesTestsRector::class,
+        ExceptionAnnotationRector::class,
+        ExplicitPhpErrorApiRector::class,
+        GetMockRector::class,
+        RequiresAnnotationWithValueToAttributeRector::class,
     ])
     ->withTypeCoverageLevel(0)
     ->withDeadCodeLevel(0)
