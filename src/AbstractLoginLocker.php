603615794d3cd9a9488b246d6706dd6ccef4c15c
Austin Passy <367897+thefrosty@users.noreply.github.com>
2025-12-01 07:56:00 -0800

    Remove unnecessary  used imports already defined by abstraction.

--- src/AbstractLoginLocker.php
+++ src/AbstractLoginLocker.php
@@ -1,25 +1,20 @@
-<?php declare(strict_types=1);
+<?php
+
+declare(strict_types=1);
 
 namespace TheFrosty\WpLoginLocker;
 
 use TheFrosty\WpUtilities\Plugin\AbstractHookProvider;
-use TheFrosty\WpUtilities\Plugin\HooksTrait;
 use TheFrosty\WpUtilities\Plugin\HttpFoundationRequestInterface;
 use TheFrosty\WpUtilities\Plugin\HttpFoundationRequestTrait;
-use TheFrosty\WpUtilities\Plugin\PluginAwareInterface;
-use TheFrosty\WpUtilities\Plugin\PluginAwareTrait;
-use TheFrosty\WpUtilities\Plugin\WpHooksInterface;
 
 /**
  * Class AbstractLoginLocker
- *
  * @package TheFrosty\WpLoginLocker
  */
 abstract class AbstractLoginLocker extends AbstractHookProvider implements
-    HttpFoundationRequestInterface,
-    PluginAwareInterface,
-    WpHooksInterface
+    HttpFoundationRequestInterface
 {
 
-    use HooksTrait, PluginAwareTrait, HttpFoundationRequestTrait;
+    use HttpFoundationRequestTrait;
 }
